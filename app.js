8ce899d4c57e1ef0931c749d618e0fbcfb7289a4
snzhr <45297635+snzhr@users.noreply.github.com>
2021-10-25 14:50:42 +0600

    Uploading files
    This calculator was built with JS on VSCode editor. Then uploading files to github.

---
+++ app.js
@@ -0,0 +1,28 @@
+const screen = document.getElementById('screen')
+let buttons = document.getElementsByClassName('btn');
+
+const allClear = document.getElementById('ac') 
+const equalBtn = document.getElementById('equal')
+let result = '';
+
+
+const returnValue=(e)=>{
+    screen.value += e.target.value;
+     result+=e.target.value;
+    
+}
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', returnValue)     
+}
+
+allClear.addEventListener('click',()=>{
+    result='';
+    screen.value = '';
+});
+
+equalBtn.addEventListener('click',()=>{
+result=eval(result);     
+screen.value =result;
+})
+
+
